ce37b14aac3664162eb03e8ea2a67b98dc91377a
Alexandr.Cherm <Alexandr.chermashentsev@gmail.com>
2022-12-28 20:55:00 +0500

    add ex_4

--- Ex_4_S4HW.py
+++ Ex_4_S4HW.py
@@ -9,4 +9,26 @@
 #        a      b    c      e     f
 # randint(0,100)
 
+import random
 
+user_degree = int(input('Enter degree: '))
+elements_of_formula = []
+for i in range(user_degree, -1, -1):
+    random_number = random.randint(0, 100)
+    if i > 1 and random_number != 0:
+        elements_of_formula.append(str(random_number) + 'x^'+ str(i))
+    elif i == 1 and random_number != 0: 
+        elements_of_formula.append(str(random_number) + 'x')
+    elif i == 0 and random_number != 0:
+        elements_of_formula.append(str(random_number))
+
+formula_str = elements_of_formula[0]
+for i in range(1, len(elements_of_formula)):
+    formula_str = formula_str + ' + ' + elements_of_formula[i]
+formula_str += ' = 0'
+
+print(elements_of_formula)
+print(formula_str)
+
+with open('formula.txt', 'a') as data:
+    data.write(f'{formula_str}\n')
